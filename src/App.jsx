36140df9dd9f8cbd40d601a8f38a78310b17849e
Rojak03 <chrisnj.ftw@gmail.com>
2025-10-30 13:51:49 +0100

    added gamedetail routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx'
+import GameDetails from './components/GameDetails.jsx';
 
 function App() {
 
   return (
-    <>
-    <Home />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gamedetails/:id" element={<GameDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
 
 export default App
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,10 +1,12 @@
-
+import { Link } from "react-router-dom";
 export default function GameCard({ game }) {
     return (
-        <a href={`./gamedetails.jsx/${game.gameID}`} className="game-card">
-            <img src={game.thumb} alt={game.external} />
-            <h2>{game.external}</h2>
-            <p>Cheapest price: ${game.cheapest}</p>
-        </a>
+        <div className="game-card">
+            <Link to={`/gamedetails/${game.gameID}`}>
+                <img src={game.thumb} alt={game.external} />
+                <h2>{game.external}</h2>
+                <p>Cheapest price: ${game.cheapest}</p>
+            </Link>
+        </div>
     );
 }
